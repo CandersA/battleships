57178f3281b660bd336c76301a28596de53ef273
CandersA <123170339+CandersA@users.noreply.github.com>
2023-03-08 12:31:27 +0200

    Changed names and lengths to ships to appropriate values in tests, added receiveAttack function

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,5 +1,13 @@
+import Ship from './ship';
+
 const gameBoard = () => {
   const boardArray = [];
+
+  const carrier = Ship(1, 'carrier');
+  const battleship = Ship(2, 'battleship');
+  const cruiser = Ship(3, 'cruiser');
+  const submarine = Ship(4, 'submarine');
+  const destroyer = Ship(5, 'destoryer');
 
   const createBoard = () => {
     for (let i = 0; i < 10; i += 1) {
@@ -19,11 +27,13 @@ const gameBoard = () => {
       if (x + ship.shipLength > 10) {
         return 'Ship is too far to the right, ship not placed';
       }
+      // This loop makes sure the ship doesn't overlap with another ship
       for (let j = 0; j < ship.shipLength; j += 1) {
         if (typeof boardArray[y][x + j] === 'string') {
           return 'Ship overlaps another ship';
         }
       }
+      // If the ship doesn't overlap place the ship in it's position
       for (let i = 0; i < ship.shipLength; i += 1) {
         boardArray[y][x + i] = ship.shipMarker;
       }
@@ -31,6 +41,7 @@ const gameBoard = () => {
       if (y + ship.shipLength > 10) {
         return 'Ship is too far down, ship not placed';
       }
+      // Here is another instance of similar code as above, but for vertical position
       for (let j = 0; j < ship.shipLength; j += 1) {
         if (typeof boardArray[y + j][x] === 'string') {
           return 'Ship overlaps another ship';
@@ -43,9 +54,33 @@ const gameBoard = () => {
     return boardArray;
   };
 
+  const missedAttacks = [];
+
+  const receiveAttack = (x, y) => {
+    const coordinates = boardArray[y][x];
+    if (typeof coordinates === 'string') {
+      if (coordinates === 'carrier') {
+        carrier.hit();
+      } else if (coordinates === 'battleship') {
+        battleship.hit();
+      } else if (coordinates === 'cruiser') {
+        cruiser.hit();
+      } else if (coordinates === 'submarine') {
+        submarine.hit();
+      } else if (coordinates === 'destroyer') {
+        destroyer.hit();
+      }
+    } else {
+      missedAttacks.push(coordinates);
+      return missedAttacks;
+    }
+    return coordinates;
+  };
+
   return {
     createBoard,
     placeShip,
+    receiveAttack,
     boardArray,
   };
 };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -16,34 +16,34 @@ describe('Test ship placement on board', () => {
   const testBoard = gameBoard();
   testBoard.createBoard();
 
-  const testShip = Ship(3, 'tanker');
-  const testShip2 = Ship(3, 'cruiser');
-  const testShip3 = Ship(3, 'destroyer');
+  const cruiser = Ship(3, 'cruiser');
+  const submarine = Ship(4, 'submarine');
+  const destroyer = Ship(5, 'destroyer');
 
   test('Ship should take the space of its length within boardArray (horizontal)', () => {
-    testBoard.placeShip(7, 5, 'horizontal', testShip);
+    testBoard.placeShip(7, 5, 'horizontal', cruiser);
 
-    expect(testBoard.boardArray[5][7]).toBe('tanker');
-    expect(testBoard.boardArray[5][8]).toBe('tanker');
-    expect(testBoard.boardArray[5][9]).toBe('tanker');
+    expect(testBoard.boardArray[5][7]).toBe('cruiser');
+    expect(testBoard.boardArray[5][8]).toBe('cruiser');
+    expect(testBoard.boardArray[5][9]).toBe('cruiser');
 
     testBoard.createBoard();
   });
 
   test('Ship should take the space of its length within boardArray (vertical)', () => {
-    testBoard.placeShip(7, 5, 'vertical', testShip);
+    testBoard.placeShip(7, 5, 'vertical', cruiser);
 
-    expect(testBoard.boardArray[5][7]).toBe('tanker');
-    expect(testBoard.boardArray[6][7]).toBe('tanker');
-    expect(testBoard.boardArray[7][7]).toBe('tanker');
+    expect(testBoard.boardArray[5][7]).toBe('cruiser');
+    expect(testBoard.boardArray[6][7]).toBe('cruiser');
+    expect(testBoard.boardArray[7][7]).toBe('cruiser');
 
     testBoard.createBoard();
   });
 
   test('Ships should not be placed if they are outside of boardArray', () => {
-    testBoard.placeShip(8, 5, 'horizontal', testShip);
-    testBoard.placeShip(8, 8, 'vertical', testShip2);
-    testBoard.placeShip(8, 10, 'vertical', testShip3);
+    testBoard.placeShip(8, 5, 'horizontal', cruiser);
+    testBoard.placeShip(8, 8, 'vertical', submarine);
+    testBoard.placeShip(8, 10, 'vertical', destroyer);
 
     expect(testBoard.boardArray[5][8]).toBe(8);
     expect(testBoard.boardArray[8][8]).toBe(8);
@@ -53,13 +53,15 @@ describe('Test ship placement on board', () => {
   });
 
   test('Ships should not overlap inside boardArray', () => {
-    testBoard.placeShip(3, 4, 'horizontal', testShip);
-    testBoard.placeShip(3, 4, 'vertical', testShip2);
-    testBoard.placeShip(5, 4, 'horizontal', testShip3);
+    testBoard.placeShip(3, 4, 'horizontal', cruiser);
+    testBoard.placeShip(3, 4, 'vertical', submarine);
+    testBoard.placeShip(4, 4, 'horizontal', destroyer);
 
-    expect(testBoard.boardArray[4][3]).toBe('tanker');
-    expect(testBoard.boardArray[4][5]).toBe('tanker');
+    expect(testBoard.boardArray[4][3]).toBe('cruiser');
+    expect(testBoard.boardArray[4][5]).toBe('cruiser');
     expect(testBoard.boardArray[5][3]).toBe(3);
     expect(testBoard.boardArray[4][7]).toBe(7);
+
+    testBoard.createBoard();
   });
 });
